84996e8d72cbccc07818c2496817f0c0d156d1d8
BibWiss <informationswissenschaftliches@gmail.com>
2023-04-23 23:06:44 +0200

    add function to change hover color to different colors

--- functions.js
+++ functions.js
@@ -23,6 +23,21 @@ toggleContainers("div-memberships", "memberships-content");
 toggleContainers("div-grants", "grants-content");
 
 toggleContainers("div-blog", "blog-content");
+    
+function changeColors() {
+  var colors = ["#743ad5", "#f637b0", "#20ffcf"];   // the color choices
+  var index = 0;                                     // index of the current color
+  link = document.getElementById("internal-link");
+  link.addEventListener("mouseover", function() {
+    link.style.color = colors[index];  // set the color of body to the current color
+      index = (index + 1) % colors.length;             // increment index to point to the next color (if it goes beyond the length of the coices array get it back to 0 using the modulo %)
+})
+link.addEventListener("mouseout", function() {
+  link.style.color = "#30dcff"
+})
+};
+
+changeColors();
 
 //old
 
