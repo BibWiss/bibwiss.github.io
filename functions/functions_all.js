d492315840149ce3fc0151d81f852258d323f072
BibWiss <informationswissenschaftliches@gmail.com>
2023-04-24 17:34:40 +0200

    fix function for link hover color change

---
+++ functions/functions_all.js
@@ -0,0 +1,23 @@
+// change link color to a selection of different colors on hover
+
+index = 0;
+
+function changeColors(link, index) {
+  var colors = ["#743ad5", "#f637b0", "#20ffcf"];   // the color choices
+  index = (index + 1) % colors.length;
+  link.addEventListener("mouseover", function() {
+    link.style.color = colors[index];
+    })
+  link.addEventListener("mouseout", function() {
+    link.style.color = "#30dcff"
+    })
+};
+
+all_links = document.links;
+
+for (var i = 0; i < all_links.length+1; i++) { 
+  var link = all_links[i];
+  index = index+1;
+  changeColors(link, index);
+  
+}
--- functions/functions_index.js
+++ functions/functions_index.js
@@ -1,3 +1,5 @@
+// buttons for (nested) collapsible containers 
+
 function toggleContainers(div, content) {
     var optionContainer = document.getElementById(div);
     var c = document.getElementById(content);
@@ -23,28 +25,3 @@ toggleContainers("div-memberships", "memberships-content");
 toggleContainers("div-grants", "grants-content");
 
 toggleContainers("div-blog", "blog-content");
-    
-function changeColors() {
-  var colors = ["#743ad5", "#f637b0", "#20ffcf"];   // the color choices
-  var index = 0;                                     // index of the current color
-  link = document.getElementById("internal-link");
-  link.addEventListener("mouseover", function() {
-    link.style.color = colors[index];  // set the color of body to the current color
-      index = (index + 1) % colors.length;             // increment index to point to the next color (if it goes beyond the length of the coices array get it back to 0 using the modulo %)
-})
-link.addEventListener("mouseout", function() {
-  link.style.color = "#30dcff"
-})
-};
-
-changeColors();
-
-//old
-
-function addContainer(div, content, name) {
-  var container = document.getElementById(div);
-  var newContainer = document.createElement("div");
-  newContainer.setAttribute("id", name);
-  newContainer.innerHTML = (content);
-  container.appendChild(newContainer);
-}
