fdd622e240724423954092264c8f6cc826ab413b
BibWiss <informationswissenschaftliches@gmail.com>
2023-05-21 20:16:58 +0200

    add function to filter by hashtag

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 /*
 TODO
-* clean up files
+* add function to reset filtering by clicking on hashtags once again
 * add dark mode
-* [optional] add filter option based on hashtags 
-* update on github
 */
 
 /*
@@ -15,16 +13,46 @@ DARK MODE
 * hashtag border color
  */
 
+// check if a specific value in the data.json has some content or is null
 function checkIfEmptyValue(prefix, val, suffix) {
     if (val != null) {return prefix + val + suffix}
     else {return ""};
 };
 
+// compare two input years (used in loop to identify when year changes)
 function compareYear(year0, year1) {
     if (year1 != year0) {return `<h3>${year0}</h3>`}
     else {return ""};
 }
 
+// filter publication entries by clicking on hashtags
+function filterByHashtag(hashtags, publications) {
+    for (var i = 0; i < hashtags.length; i++) {
+        var button = hashtags[i];
+        let text = button.textContent;
+        
+        button.onclick = function(){
+            this.classList.remove("inactive");
+            this.classList.add("active");
+            // change style for all hashtags with same content
+            const similarPubs = document.querySelectorAll(`.${'hashtag-' + text}`);
+            for (const sPub of similarPubs) {
+                sPub.classList.remove("inactive");
+                sPub.classList.add("active");
+            };
+            const otherPubs = document.querySelectorAll(`.show-pub:not(.${text})`);
+            console.log(otherPubs)
+            for (const oPub of otherPubs) {
+                oPub.classList.remove("show-pub");
+                oPub.classList.add("hide-pub");
+            }
+        };
+}
+};
+
+// add funct to change active back to inactive on click
+
+// get all metadata about publications from "data.json" file
 function getMetadata(path) {
     var passageContainer = document.getElementById("app");
     var jsondata;
@@ -38,9 +66,14 @@ function getMetadata(path) {
             for (var i = Object.keys(jsondata.title).length; i--; i >= 0) {
                 var div = document.createElement("div");
                 div.setAttribute("id", "div_" + (i));
-                div.innerHTML = (`${compareYear(jsondata["year"][i], year0)}<p>${checkIfEmptyValue("<span class='authors'>", jsondata["co-author(s)"][i], "</span>: ")}<span class="title"><a href="${jsondata["link to publication"][i]}">${jsondata["title"][i]}</a></span>${checkIfEmptyValue(", in <span class='publisher'>",  jsondata["publisher (e.g. journal/blog name)"][i], "</span>")}. ${checkIfEmptyValue("<span class='hashtag'>#",jsondata["type"][i],"</span>")} ${checkIfEmptyValue("<span class='hashtag'>#", jsondata["community"][i], "</span>")}</p>`);
+                div.innerHTML = (`${compareYear(jsondata["year"][i], year0)}<p class="show-pub ${checkIfEmptyValue("",jsondata["type"][i],"")} ${checkIfEmptyValue("", jsondata["community"][i], "")}">${checkIfEmptyValue("<span class='authors'>", jsondata["co-author(s)"][i], "</span>: ")}<span class="title"><a href="${jsondata["link to publication"][i]}">${jsondata["title"][i]}</a></span>${checkIfEmptyValue(", in <span class='publisher'>",  jsondata["publisher (e.g. journal/blog name)"][i], "</span>")}. ${checkIfEmptyValue(`<span class='hashtag hashtag-${jsondata["type"][i]} inactive'>`,jsondata["type"][i],"</span>")} ${checkIfEmptyValue(`<span class='hashtag hashtag-${jsondata["community"][i]} inactive'>`, jsondata["community"][i], "</span>")}</p>`);
                 passageContainer.appendChild(div);
                 var year0 = jsondata["year"][i];
+                //test
+                var inactive_hashtags = document.getElementsByClassName('inactive');
+                var publications = document.getElementsByClassName('show-pub');
+                filterByHashtag(inactive_hashtags, publications);
+                //hashtags[0].innerHTML = "HELLO WORLD";
             };
         }
     );
@@ -49,3 +82,9 @@ function getMetadata(path) {
 };
 
 getMetadata("data.json");
+
+
+
+
+
+
